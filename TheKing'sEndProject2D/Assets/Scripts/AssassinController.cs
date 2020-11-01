7e5d44d32b0cd22fc77a277017ac9e579cec79e6
Lyle <lylespurrell@hotmail.com>
2020-11-01 06:09:17 -0500

    Assassin prefab with animations

--- TheKing'sEndProject2D/Assets/Scripts/AssassinController.cs
+++ TheKing'sEndProject2D/Assets/Scripts/AssassinController.cs
@@ -9,13 +9,19 @@ public class AssassinController : MonoBehaviour
     [SerializeField] float jumpForce = 7.5f;
     [SerializeField] float rollForce = 6.0f;
     [SerializeField] float gravityScale = 0.5f;
+    [SerializeField] GameObject slideDust;
 
+    private Animator animator;
     private Rigidbody2D rbody;
     private Transform AssassinDirection;
     private AssassinColliders assassinColliders;
+    private Transform spawnDustR;
 
+    private bool attackAnimationReady = true;
+    private bool idleBlockAnimationReady = true;
     private bool extrajump = true;
     private bool rollingIsReady = true;
+    private bool rollingAnimationIsReady = true;
     private bool rolling = false;
     private bool onWall = false;
     private float inputX;
@@ -23,9 +29,11 @@ public class AssassinController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         rbody = GetComponent<Rigidbody2D>();
         AssassinDirection = transform;
         assassinColliders = transform.Find("Assassin_Ground_Collider").GetComponent<AssassinColliders>();
+        spawnDustR = transform.Find("Assassin_Wall_Slide_R").GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -66,14 +74,15 @@ public class AssassinController : MonoBehaviour
         // Wall Sliding    
         if (onWall == false && assassinColliders.GroundedState() == false && assassinColliders.SlidingState() == true && rbody.velocity.y < 0)
         {
-            if (AssassinDirection.localScale.x == 1)
+            if (AssassinDirection.localScale.x == -1)
             {
-                AssassinDirection.localScale = new Vector2(-1, 1);
+                Instantiate(slideDust, spawnDustR.position, gameObject.transform.localRotation);
             }
-            else if (AssassinDirection.localScale.x == -1)
+            else if (AssassinDirection.localScale.x == 1)
             {
-                AssassinDirection.localScale = new Vector2(1, 1);
+                Instantiate(slideDust, spawnDustR.position, gameObject.transform.localRotation);
             }
+
             rbody.gravityScale = gravityScale;
             onWall = true;
             extrajump = true;
@@ -85,20 +94,12 @@ public class AssassinController : MonoBehaviour
         }
         else if (onWall == true && assassinColliders.GroundedState() == true)
         {
-            if (AssassinDirection.localScale.x == 1)
-            {
-                AssassinDirection.localScale = new Vector2(-1, 1);
-            }
-            else if (AssassinDirection.localScale.x == -1)
-            {
-                AssassinDirection.localScale = new Vector2(1, 1);
-            }
             rbody.gravityScale = gravityScale * 2;
             onWall = false;
         }
 
         // Jump
-        if (Input.GetKeyDown(KeyCode.W) && assassinColliders.GroundedState() || Input.GetKeyDown(KeyCode.W) && extrajump == true)
+        if (Input.GetKeyDown(KeyCode.W) && assassinColliders.GroundedState() == true || Input.GetKeyDown(KeyCode.W) && extrajump == true)
         {
             rbody.gravityScale = gravityScale * 2;
             rbody.velocity = new Vector2(rbody.velocity.x, jumpForce);
@@ -106,10 +107,10 @@ public class AssassinController : MonoBehaviour
         }
 
         // Attack
-        if (Input.GetMouseButtonDown(0))
-        {
-            Debug.Log("Attack");
-        }
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Debug.Log("Attack");
+        //}
 
         // Block
         if (Input.GetMouseButtonDown(1))
@@ -117,12 +118,118 @@ public class AssassinController : MonoBehaviour
             Debug.Log("Block");
         }
 
-        // Counter attack on perfect block
+        // Counter attack on perfect block      
+
+        // Jump animation 
+        if (rbody.velocity.y > 0)
+        {
+            animator.SetBool("Jump", true);
+        }
+        else if (rbody.velocity.y < 0)
+        {
+            animator.SetBool("Jump", false);
+            animator.SetBool("Fall", true);
+        }
+        else if (rbody.velocity.y == 0)
+        {
+            animator.SetBool("Fall", false);
+        }
+
+        // Run animation
+        if (assassinColliders.GroundedState() == true)
+        {
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+            {
+                animator.SetBool("Run", true);
+            }
+            else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+            {
+                animator.SetBool("Run", false);
+            }
+        }
+        else if (assassinColliders.GroundedState() == false)
+        {
+            if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+            {
+                animator.SetBool("Run", false);
+            }
+        }
+
+        // Wall slide animation
+        if (assassinColliders.SlidingState() == true && assassinColliders.GroundedState() == true)
+        {
+            animator.SetBool("Wall Slide", false);
+        }
+        else if (assassinColliders.SlidingState() == true)
+        {
+            animator.SetBool("Wall Slide", true);
+        }
+        else if (assassinColliders.SlidingState() == false)
+        {
+            animator.SetBool("Wall Slide", false);
+        }
+
+        // Roll animation
+        if (rollingAnimationIsReady == true && Input.GetKeyUp("left shift"))
+        {
+            rollingAnimationIsReady = false;
+            animator.SetTrigger("Roll");
+            Invoke("RollAnimationCooldown", 1);
+        }
+
+        // Attack animations
+        if (attackAnimationReady == true && Input.GetMouseButtonDown(0))
+        {
+            attackAnimationReady = false;
+            AttackAnimations(Random.Range(0, 3));
+            Invoke("AttackAnimationCooldown", 1);
+        }
+
+        // Idle block animation
+        if (idleBlockAnimationReady == true && Input.GetMouseButtonDown(1))
+        {            
+            idleBlockAnimationReady = false;
+            animator.SetTrigger("Idle Block");
+            Invoke("IdleBlockAnimationCooldown", 1);
+        }
+
+        // Block attack animation
     }
 
     void RollCooldown()
     {
         rollingIsReady = true;
         rolling = false;
+    }
+
+    void RollAnimationCooldown()
+    {
+        rollingAnimationIsReady = true;
+    }
+
+    void AttackAnimationCooldown()
+    {
+        attackAnimationReady = true;
+    }
+
+    void AttackAnimations(int num)
+    {
+        if (num == 0)
+        {
+            animator.SetTrigger("Attack 1");
+        }
+        else if (num == 1)
+        {
+            animator.SetTrigger("Attack 2");
+        }
+        else if (num == 2)
+        {
+            animator.SetTrigger("Attack 3");
+        }
+    }
+
+    void IdleBlockAnimationCooldown()
+    {
+        idleBlockAnimationReady = true;
     }
 }
