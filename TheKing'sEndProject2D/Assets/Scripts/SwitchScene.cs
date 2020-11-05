1b3ec05d608c4ef922f32890967cd8f60399d203
Al Hafeez <aragornlotr954@gmail.com>
2020-11-05 18:15:12 -0500

    Added a Main Menu Scene with 3 buttons,

---
+++ TheKing'sEndProject2D/Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+ 
+  
+
+    public void onPlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void onOptionsButton()
+    {
+
+    }
+
+     public void onExitButton()
+    {
+        Application.Quit();
+    }
+}
