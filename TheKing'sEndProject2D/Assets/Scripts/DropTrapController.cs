8ba5dd842178fe36a7740b369f5d4242b0584d85
Peitong Liu <vincentliu9816@gmail.com>
2020-11-05 21:04:39 -0500

    Added Dropping Trap, Peitong Liu
    Unable to test for the camera is not moving along with the player.

---
+++ TheKing'sEndProject2D/Assets/Scripts/DropTrapController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropTrapController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            Invoke("DropPlat", .1f);
+            Destroy(gameObject, 2f);
+        }
+    }
+    void DropPlat()
+    {
+        rb.isKinematic = false;
+    }
+}
