020554d3a13256fe140c0541ffcd4f9e50d5e987
Al Hafeez <aragornlotr954@gmail.com>
2020-11-10 12:28:03 -0500

    Auto stash before merge of "main" and "origin/main"

---
+++ TheKing'sEndProject2D/Assets/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour
+{
+    public Slider healthSlider;
+
+    void SetHealth(float health)
+    {
+        healthSlider.value = health;
+    }
+
+    
+}
