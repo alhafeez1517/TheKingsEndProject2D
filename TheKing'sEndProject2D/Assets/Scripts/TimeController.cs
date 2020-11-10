eceb43db83634cb29d1839f69ec03c65518e9c5e
Al Hafeez <aragornlotr954@gmail.com>
2020-11-10 15:43:46 -0500

    Added a timer in level 1

---
+++ TheKing'sEndProject2D/Assets/Scripts/TimeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeController : MonoBehaviour
+{
+   public Text timeCounter;
+    public TimeSpan timeSpan;
+    public bool continueTime;
+    public float elapsedTime;
+
+    void Start()
+    {
+        timeCounter.text = "00:00";
+        StartTime();
+        //continueTime = false;
+        
+    }
+
+    void StartTime()
+    {
+        continueTime = true;
+        elapsedTime = 0;
+        StartCoroutine(UpdateTimer());
+
+    }
+
+    void EndTime()
+    {
+        continueTime = false;
+    }
+
+    private IEnumerator UpdateTimer()
+    {
+        while (continueTime)
+        {
+            elapsedTime += Time.deltaTime;
+            timeSpan = TimeSpan.FromSeconds(elapsedTime);
+            string currentTime = "Time: "+ timeSpan.ToString("mm':'ss");
+            timeCounter.text = currentTime;
+            yield return null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
