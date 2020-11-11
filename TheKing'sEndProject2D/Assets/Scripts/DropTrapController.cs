879afe48c84615c23502447e00d2be677cb52c9d
Lyle <lylespurrell@hotmail.com>
2020-11-11 05:24:29 -0500

    Improved Dropping Platform Trap
    - Can loop the platform to return
    - Fixed player double health point damage bug

--- TheKing'sEndProject2D/Assets/Scripts/DropTrapController.cs
+++ TheKing'sEndProject2D/Assets/Scripts/DropTrapController.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class DropTrapController : MonoBehaviour
 {
-    [SerializeField] float timeBeforeDrop = 1f;
-    [SerializeField] float timeToDestroy = 2f;
-    [SerializeField] bool destroyOnContact = false;
+    [SerializeField] float timeBeforeDrop = 0.5f;
+    [SerializeField] float delayTime = 1f;
+    [SerializeField] bool isReturning = true;
 
-    Rigidbody2D rb;
+    private float originalPositionY;
+    private Rigidbody2D rb;
+    private bool changeToTransparent = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        originalPositionY = transform.position.y;
         rb = GetComponent<Rigidbody2D>();
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag.Equals("Player"))
@@ -21,18 +26,38 @@ public class DropTrapController : MonoBehaviour
             Invoke("DropPlat", timeBeforeDrop);
         }
 
-        if (destroyOnContact == true)
-        {
-            if (collision.gameObject.tag.Equals("Ground") || collision.gameObject.tag.Equals("Trap") || collision.gameObject.tag.Equals("Wall"))
-            {
-                Destroy(gameObject);
-            }
-        }
+        //if (isReturning == false)
+        //{
+        //    if (collision.gameObject.tag.Equals("Ground") || collision.gameObject.tag.Equals("Trap") || collision.gameObject.tag.Equals("Wall"))
+        //    {
+        //        Destroy(gameObject);
+        //    }
+        //}
     }
     void DropPlat()
     {
-        //GetComponent<Collider2D>().enabled = false;
-        rb.isKinematic = false;
-        Destroy(gameObject, timeToDestroy);
+        GetComponent<Collider2D>().enabled = false;
+        rb.isKinematic = false;   
+
+        if (isReturning == true)
+        {
+            Invoke("Return", delayTime);
+        }
+        else if (isReturning == false)
+        {
+            Invoke("DestroyPlatform", delayTime);
+        }
+    }
+
+    void Return()
+    {
+        GetComponent<Collider2D>().enabled = true;
+        rb.bodyType = RigidbodyType2D.Static;
+        transform.position = new Vector2(transform.position.x, originalPositionY);
+    }
+
+    void DestroyPlatform()
+    {
+        Destroy(gameObject);
     }
 }
