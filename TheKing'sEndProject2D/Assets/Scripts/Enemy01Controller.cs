2722982598836feb07784de2b53a14b65f853dd1
Peitong Liu <vincentliu9816@gmail.com>
2020-11-30 02:09:14 -0500

    Enemy Script added

--- TheKing'sEndProject2D/Assets/Scripts/Enemy01Controller.cs
+++ TheKing'sEndProject2D/Assets/Scripts/Enemy01Controller.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class Enemy01Controller : MonoBehaviour
 {
+    //Adjustables
+    [SerializeField] int enemyHelath = 3;
+    [SerializeField] int enemyCurrentHealth;
+    [SerializeField] float enemySpeed = 3f;
+    [SerializeField] float invokeIdleDist = 10f;
+    [SerializeField] float invokeChaseDist = 5f;
+    [SerializeField] float invokeAttackDist = 3f;
+    //Public parameters.
+    public int enemyState;
+    //Private parameters.
+    private Animator eAnimator;
+    private Rigidbody2D eRb;
+    private BoxCollider2D eBxColl;
+    private Transform eTransform;
+    private enum states{IDLE,PATROL,CHASE,DASH,ATTACK}
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyState = (int)states.IDLE;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckState();
+    }
+    void CheckState()
+    {
+        switch (states)
+        {
+            case states.IDLE:
+                EnemyIdle();
+                break;
+            case states.PATROL:
+                EnemyPatrol();
+                break;
+            case states.CHASE:
+                EnemyChase();
+                break;
+            case states.DASH:
+                EnemyDash();
+                break;
+            case states.ATTACK:
+                EnemyAttack();
+                break;
+        }
     }
 }
