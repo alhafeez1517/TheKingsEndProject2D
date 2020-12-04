aa588ea0ce1dc97e490690d3c9cafb2acec27d8c
Al Hafeez <aragornlotr954@gmail.com>
2020-12-04 00:51:35 -0500

    Added Score Canvas and fixed dropping platform issue
    Added Score Canvas.Currently not functional.Also fixed the issue where the dropping platforms didn't respawn.

---
+++ TheKing'sEndProject2D/Assets/Scripts/LevelComplete.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LevelComplete : MonoBehaviour
+{
+    #region Texts
+    public TMP_Text levelComplete;
+    public TMP_Text timesDied;
+    public TMP_Text enemieskilled;
+    public TMP_Text timeTaken;
+    #endregion
+
+    #region Buttons
+    public Button mainMenuButton;
+    public Button nextLevelButton;
+    #endregion
+
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void OnMainMenuClick()
+    {
+        
+    }
+
+    public void OnNextLevelClick()
+    {
+        
+    }
+}
