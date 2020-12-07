cedf2a5c779a3ade468263055cc366586742618c
SaltyFish486 <171323947@qq.com>
2020-12-07 04:20:14 -0500

    projectile trap created, level2 and 3 design is done

---
+++ TheKing'sEndProject2D/Assets/Scripts/Arrow_Script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow_Script : MonoBehaviour
+{
+    [SerializeField] public float speed;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+        Destroy(gameObject, 4f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TheKing'sEndProject2D/Assets/Scripts/projectile_trap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile_trap : MonoBehaviour
+{
+    [SerializeField] public Transform firepoint;
+    [SerializeField] public GameObject arrow;
+     float timebetween;
+    [SerializeField] public float startTimeBetween;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timebetween = startTimeBetween;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timebetween <= 0)
+        {
+            Instantiate(arrow, firepoint.position, firepoint.rotation);
+            timebetween = startTimeBetween;
+        }
+        else
+        {
+            timebetween -= Time.deltaTime;
+        }
+        
+    }
+}
